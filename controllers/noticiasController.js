2b4eab9a7bcb370cf8c6b3a41ebc12a6ab1952ac
Guille <51679006+GuiSchet@users.noreply.github.com>
2024-01-31 15:08:38 +0000

    cambio filtro d noticias

--- controllers/noticiasController.js
+++ controllers/noticiasController.js
@@ -12,11 +12,25 @@ const guardarNoticia = async (a, b, c, d, e, f) => {
 };
 
 const guardarTodasNoticias = async (listaNoticias) => {
+    const fechaHoy = new Date().toISOString().split('T')[0]; // Obtener la fecha actual en formato YYYY-MM-DD
+
     for (const noticia of listaNoticias) {
-        if (noticia.puntaje > 1) {
+        if (noticia.puntaje > 0 && noticia.fechaPublicacion && noticia.fechaPublicacion.startsWith(fechaHoy)) {
             console.log(noticia);
+            try {
+                await guardarNoticia(
+                    noticia.titulo, 
+                    noticia.enlace, 
+                    noticia.descripcion, 
+                    noticia.imagen,
+                    noticia.fechaPublicacion,
+                    noticia.puntaje
+                );
+            } catch (error) {
+                console.error('Error al guardar noticia en servidor: ', error);
+            }
         }
     }
-}
+};
 
 export {guardarNoticia, guardarTodasNoticias}
